96e2640d12de2d2e8d45bc093708452e6be0ec0b
Taylor Reid <taylorsreid@gmail.com>
2023-03-23 10:38:04 -1000

    Making and confirming res fully functional.

--- js/confirmReservation.js
+++ js/confirmReservation.js
@@ -9,20 +9,18 @@ if(Cookies.get("loggedIn") !== "true"){
     setTimeout(function(){window.location.href = "./login.html"}, 3000); //callback to wait 3 seconds then redirect
 }
 
-const urlParams = new URLSearchParams(window.location.search);
+const checkInLocalDate = sessionStorage.getItem("checkInLocalDate");
+const checkOutLocalDate = sessionStorage.getItem("checkOutLocalDate");
+const checkInIsoDate = new Date(checkInLocalDate).toISOString().split("T")[0]; //API only accepts ISO dates, split("T")[0] removes the time
+const checkOutIsoDate = new Date(checkOutLocalDate).toISOString().split("T")[0]; //API only accepts ISO dates, split("T")[0] removes the time
+const roomSize = sessionStorage.getItem("roomSize");
+const wifi = sessionStorage.getItem("wifi");
+const breakfast = sessionStorage.getItem("breakfast");
+const parking = sessionStorage.getItem("parking");
+const guests = sessionStorage.getItem("guests");
 
-const checkInLocal = urlParams.get("checkIn");
-const checkOutLocal = urlParams.get("checkOut");
-const checkInIso = new Date(checkInLocal).toISOString().split("T")[0]; //API only accepts ISO dates, split("T")[0] removes the time
-const checkOutIso = new Date(checkOutLocal).toISOString().split("T")[0]; //API only accepts ISO dates, split("T")[0] removes the time
-const roomSize = urlParams.get("roomSize");
-const wifi = urlParams.get("wifi");
-const breakfast = urlParams.get("breakfast");
-const parking = urlParams.get("parking");
-const guests = urlParams.get("guests");
-
-document.getElementById("checkIn").insertAdjacentHTML("beforeend", checkInLocal);
-document.getElementById("checkOut").insertAdjacentHTML("beforeend", checkOutLocal);
+document.getElementById("checkIn").insertAdjacentHTML("beforeend", checkInLocalDate);
+document.getElementById("checkOut").insertAdjacentHTML("beforeend", checkOutLocalDate);
 document.getElementById("roomSize").insertAdjacentHTML("beforeend", roomSize);
 document.getElementById("wifi").insertAdjacentHTML("beforeend", wifi);
 document.getElementById("breakfast").insertAdjacentHTML("beforeend", breakfast);
@@ -39,8 +37,8 @@ submitButton.addEventListener("click", function(){
             Authorization : 'Bearer ' + Cookies.get('jwt'),
         },
         body : JSON.stringify({
-            checkIn : checkInIso,
-            checkOut : checkOutIso,
+            checkIn : checkInIsoDate, //you must send the date as ISO format
+            checkOut : checkOutIsoDate, //you must send the date as ISO format
             roomSize : roomSize,
             wifi : wifi,
             breakfast : breakfast,
@@ -51,7 +49,9 @@ submitButton.addEventListener("click", function(){
     .then(resp => resp.json())
     .then(json => {
         if (json.success == true) {
-            
+            document.getElementById("submitButton").disabled = true;
+            sessionStorage.clear();
+            document.getElementById("formMessage").innerHTML = json.message;
         }
         else {
             console.log(json);
