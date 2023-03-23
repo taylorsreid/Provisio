c5928251690f9449404a23c2222c54495990dc89
Taylor Reid <taylorsreid@gmail.com>
2023-03-23 10:38:04 -1000

    Making and confirming res fully functional.

--- js/bookReservation.js
+++ js/bookReservation.js
@@ -13,30 +13,54 @@ if(Cookies.get("loggedIn") !== "true"){
 
 //jquery datepicker adds calendar popout to date inputs
 $(".date").datepicker({
-    onSelect: function(dateText) {
-
-        let checkIn = $("#checkIn").datepicker("getDate");
-        let checkOut = $("#checkOut").datepicker("getDate");
-
-        //prevents a null error from being thrown in the console
-        if(checkIn !== null && checkOut !== null){
-
-            //ensures that the user is picking a date that's not in the past, new Date().setHours(0,0,0,0) returns today at midnight
-            if(checkIn >= new Date().setHours(0,0,0,0)){
-
-                let deltaTime = checkOut.getTime() - checkIn.getTime();
-                let deltaDays = deltaTime / (1000 * 3600 * 24);
-
-                //ensures that the user picks at least a one night stay and not a negative number of nights
-                if(checkOut > checkIn){
-                    submitButton.disabled = false;
-                    formMessage.innerHTML = `You will earn ${deltaDays * 150} points for this trip.`;
-                }
-                else{
-                    submitButton.disabled = true;
-                    formMessage.innerHTML = `You must select at least a one night stay.`;
-                }
-            }
-        } 
+    onClose : function(dateText){
+        updatePoints();
     }
+});
+
+function updatePoints(){
+
+    let checkIn = $('#checkIn').datepicker('getDate');
+    let checkOut = $('#checkOut').datepicker('getDate')
+
+    //prevents a null error from being thrown in the console
+    if(checkIn !== null && checkOut !== null){
+
+        //ensures that the user is picking a date that's not in the past, new Date().setHours(0,0,0,0) returns today at midnight
+        if(checkIn >= new Date().setHours(0,0,0,0)){
+
+            let deltaTime = checkOut.getTime() - checkIn.getTime();
+            let deltaDays = deltaTime / (1000 * 3600 * 24);
+
+            //ensures that the user picks at least a one night stay and not a negative number of nights
+            if(checkOut > checkIn){
+                submitButton.disabled = false;
+                formMessage.innerHTML = `You will earn ${deltaDays * 150} points for this trip.`;
+            }
+            else{
+                submitButton.disabled = true;
+                formMessage.innerHTML = `You must select at least a one night stay.`;
+            }
+        }
+        else{
+            submitButton.disabled = true;
+            formMessage.innerHTML = `You must select a check in date greater than or equal to today.`;
+        }
+    }
+}
+
+document.getElementById("submitButton").addEventListener("click", function(){
+
+    sessionStorage.setItem("checkInLocalDate", $("#checkIn").val());
+    sessionStorage.setItem("checkOutLocalDate", $("#checkOut").val());
+    sessionStorage.setItem("roomSize", document.getElementById("roomSize").value);
+    sessionStorage.setItem("wifi", document.getElementById("wifi").value);
+    sessionStorage.setItem("breakfast", document.getElementById("breakfast").value);
+    sessionStorage.setItem("parking", document.getElementById("parking").value);
+    sessionStorage.setItem("guests", document.getElementById("guests").value);
+
+    console.log($('#checkIn').datepicker('getDate'));
+    console.log($('#checkOut').datepicker('getDate'));
+    window.location.href = "./confirmReservation.html";
+
 });
