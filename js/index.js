4cce5306d77cbdc5a5bc690e46f12b028e97b1fd
Taylor Reid <taylorsreid@gmail.com>
2023-03-23 20:50:24 +0000

    Booking is functional

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/+esm';
 
-if(Cookies.get("loggedIn") === "true"){
-    // 
-    document.getElementById("welcomeMessage").innerHTML = `Welcome ${Cookies.get('firstName')} ${Cookies.get('lastName')}`; //super buggy
-    console.log(`${Cookies.get('firstName')} ${Cookies.get('lastName')}`);
+if(Cookies.get("loggedIn") === "true"){ //cookies returns a string not a boolean
+    //echo users name if they're logged in
+    document.getElementById("welcomeMessage").innerHTML = `Welcome ${Cookies.get('firstName')} ${Cookies.get('lastName')}`;
 }
 else{
     document.getElementById("welcomeMessage").innerHTML = "Welcome, please log in.";
