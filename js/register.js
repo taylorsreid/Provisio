5118027e8d0fd2f910b16db9601ed022d7c6a4d1
Taylor Reid <taylorsreid@gmail.com>
2023-03-26 01:45:51 -1000

    Moved email and password validation back to the backend. Backend now responds with reasons for registration failure and frontend simply displays backend's message.  Fixed API calls in register.js  Register service fixed.  SQL creation script updated.

--- js/register.js
+++ js/register.js
@@ -19,63 +19,34 @@ submitButton.addEventListener("click", function(){
     let lastName = document.getElementById("lastName").value;
     let password = document.getElementById("password").value;
 
-    //validate email and password via regex - credit to https://www.ocpsoft.org/tutorials/regular-expressions/password-regular-expression/ for the explanation of how to write regex
-    let validEmail = email.match("@{1}") !== null; //only checks for the @ symbol
-    let validPassword = password.match("^(?=.*[a-z])(?=.*[A-Z]).{8,}$") !== null; //checks if the password is at least 8 characters in length and includes one uppercase and one lowercase letter
-
-    //validate that first name and last name are not blank since HTML required attribute does not work correctly with a JS EventListener
-    let validFirstName = firstName !== "";
-    let validLastName = lastName !== "";
-
-    //if everything matches then build request to API
-    if (validEmail && validPassword && validFirstName && validLastName) {
-        fetch(apiLocation + 'register', {
-            method : "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body : JSON.stringify({
-                email : email,
-                firstName : firstName,
-                lastName : lastName,
-                password : password
-            })
+    //build request to API
+    fetch(apiLocation + 'register', {
+        method : "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify({
+            email : email,
+            firstName : firstName,
+            lastName : lastName,
+            password : password
         })
-        .then(resp => resp.json())
-        .then(json => {
-            //if the API responds that the account was created successfully, then save commonly accessed items to a cookie to avoid repeated database calls
-            if (json.success == true) {
-                Cookies.set("loggedIn", true, {expires : 1});
-                Cookies.set("customerId", json.customerId, {expires : 1});
-                Cookies.set("email", json.email, {expires : 1});
-                Cookies.set("firstName", json.firstName, {expires : 1});
-                Cookies.set("lastName", json.lastName, {expires : 1});
-                Cookies.set('jwt', json.jwt, {expires : 1}) //save JWT to a cookie because it's the most secure way
-                window.location.href = "index.html"; //redirect back to home page upon successful login
-            }
-            else {
-                formMessage.innerHTML = json.message;
-            }
-        })
-    }
-    //if the email or password doesn't meet requirements
-    else {
-        formMessage.innerHTML = ""; //blank out message box so messages don't pile up
-        if(!validEmail){
-            formMessage.insertAdjacentHTML('beforeend', `"${email}" is not a valid email.<br>`);
+    })
+    .then(resp => resp.json())
+    .then(json => {
+        //if the API responds that the account was created successfully, then save commonly accessed items to a cookie to avoid repeated database calls
+        //API responds to successful register calls with a login request response
+        if (json.success == true) {
+            Cookies.set("loggedIn", true, {expires : 1});
+            Cookies.set("customerId", json.customerId, {expires : 1});
+            Cookies.set("email", email, {expires : 1}); //not sent back by the API since the user has already entered it
+            Cookies.set("firstName", json.firstName, {expires : 1});
+            Cookies.set("lastName", json.lastName, {expires : 1});
+            Cookies.set('jwt', json.jwt, {expires : 1}) //save JWT to a cookie because it's the most secure way
+            window.location.href = "./index.html"; //redirect back to home page upon successful login
         }
-        if(!validFirstName){
-            formMessage.insertAdjacentHTML('beforeend', `First name cannot be blank.<br>`);
+        else {
+            formMessage.innerHTML = json.message;
         }
-        if(!validLastName){
-            formMessage.insertAdjacentHTML('beforeend', `Last name cannot be blank.<br>`);
-        }
-        if(!validPassword){
-            formMessage.insertAdjacentHTML('beforeend', `
-                Your password does not meet requirements.
-                It must be least 8 characters in length and include one uppercase letter and one lowercase letter.<br>
-            `);
-        }
-    }
-
+    })
 });
