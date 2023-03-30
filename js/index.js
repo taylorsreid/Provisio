8ee178201a54dd1b1eb0c92ed3ad4af9381f85f8
Taylor Reid <taylorsreid@gmail.com>
2023-03-30 03:49:14 -1000

    Removed personal frontend tests from main.

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/+esm';
-
-if(Cookies.get("loggedIn") === "true"){ //cookies returns a string not a boolean
-    //echo users name if they're logged in
-    document.getElementById("welcomeMessage").innerHTML = `Welcome ${Cookies.get('firstName')} ${Cookies.get('lastName')}`;
-}
-else{
-    document.getElementById("welcomeMessage").innerHTML = "Welcome, please log in.";
-}
