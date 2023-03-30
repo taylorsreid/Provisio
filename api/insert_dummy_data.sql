3ec5c9c3e5505c0ece54d34b386ff8028dd11b21
Taylor Reid <taylorsreid@gmail.com>
2023-03-29 17:47:12 -1000

    update dummy data

--- api/insert_dummy_data.sql
+++ api/insert_dummy_data.sql
@@ -20,9 +20,9 @@ VALUES('79657b19-cd22-11ed-a4d2-1735c641f2fc', 'testuseriii@example.com', 'Test'
 INSERT INTO `reservations` (`reservation_id`, `user_id`, `location_id`, `check_in`, `check_out`, `room_size_id`, `wifi`, `breakfast`, `parking`)
 VALUES('046be693-9145-4d45-a63d-034ae708725a', 'f81c3fe5-cc03-11ed-a4d2-1735c641f2fc', 1, '2024-04-01', '2024-04-08', 3, 1, 1, 1);
 INSERT INTO `reservations` (`reservation_id`, `user_id`, `location_id`, `check_in`, `check_out`, `room_size_id`, `wifi`, `breakfast`, `parking`)
-VALUES('179dc8db-a8ae-423e-9a74-e42a44900e65', '67583904-cd22-11ed-a4d2-1735c641f2fc', 2, '2024-04-02', '2024-04-09', 1, 1, 1, 0);
+VALUES('179dc8db-a8ae-423e-9a74-e42a44900e65', '67583904-cd22-11ed-a4d2-1735c641f2fc', 2, '2024-04-02', '2024-04-08', 1, 1, 1, 0);
 INSERT INTO `reservations` (`reservation_id`, `user_id`, `location_id`, `check_in`, `check_out`, `room_size_id`, `wifi`, `breakfast`, `parking`)
-VALUES('25d3c9b6-f572-4dad-96e2-0b31317759f6', '79657b19-cd22-11ed-a4d2-1735c641f2fc', 3, '2024-04-03', '2024-04-10', 4, 1, 0, 0);
+VALUES('25d3c9b6-f572-4dad-96e2-0b31317759f6', '79657b19-cd22-11ed-a4d2-1735c641f2fc', 3, '2024-04-03', '2024-04-08', 4, 1, 0, 0);
 
 -- create dummy guests, grouped by reservation
 INSERT INTO `guests` (guest_id, reservation_id, first_name, last_name)
