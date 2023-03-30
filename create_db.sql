2d803cbfdabab880bcbc9e007a6e760cafe0b319
Taylor Reid <taylorsreid@gmail.com>
2023-03-30 03:58:31 -1000

    Renamed locations to hotels so that it doesn't interfere with the JS reserved word.

--- create_db.sql
+++ create_db.sql
@@ -26,10 +26,10 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- create table of hotels, reduces repeated varchar strings in reservations table
-CREATE TABLE `locations` (
-  `location_id` tinyint UNIQUE NOT NULL,
-  `location_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`location_id`)
+CREATE TABLE `hotels` (
+  `hotel_id` tinyint UNIQUE NOT NULL,
+  `hotel_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`hotel_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- create table of room types, reduces repeated varchar strings in reservations table
@@ -43,7 +43,7 @@ CREATE TABLE `room_sizes` (
 CREATE TABLE `reservations` (
   `reservation_id` varchar(36) UNIQUE NOT NULL, -- UUID
   `user_id` varchar(36) NOT NULL, -- UUID
-  `location_id` tinyint NOT NULL,
+  `hotel_id` tinyint NOT NULL,
   `check_in` date NOT NULL,
   `check_out` date NOT NULL,
   `room_size_id` tinyint NOT NULL,
@@ -52,7 +52,7 @@ CREATE TABLE `reservations` (
   `parking` boolean DEFAULT NULL,
   PRIMARY KEY (`reservation_id`),
   FOREIGN KEY (`user_id`) REFERENCES users(`user_id`),
-  FOREIGN KEY (`location_id`) REFERENCES locations(`location_id`),
+  FOREIGN KEY (`hotel_id`) REFERENCES hotels(`hotel_id`),
   FOREIGN KEY (`room_size_id`) REFERENCES room_sizes(`room_size_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -62,7 +62,7 @@ CREATE OR REPLACE VIEW `reservations_view` AS SELECT
 `user_id`,
 (SELECT `first_name` FROM `users` WHERE `reservations`.`user_id` = `users`.`user_id`) AS 'first_name',
 (SELECT `last_name` FROM `users` WHERE `reservations`.`user_id` = `users`.`user_id`) AS 'last_name',
-(SELECT `location_name` FROM `locations` WHERE `reservations`.`location_id` = `locations`.`location_id`) AS 'location_name',
+(SELECT `hotel_name` FROM `hotels` WHERE `reservations`.`hotel_id` = `hotels`.`hotel_id`) AS 'hotel_name',
 `check_in`,
 `check_out`,
 (SELECT `room_size_name` FROM `room_sizes` WHERE `reservations`.`room_size_id` = `room_sizes`.`room_size_id`) AS 'room_size_name',
@@ -83,11 +83,11 @@ CREATE TABLE `guests` (
 
 -- dummy data is created in a separate file, everything in this file is necessary for the database and API to function
 
--- insert necessary location data
-INSERT INTO `locations` (`location_id`, `location_name`) VALUES (1, "Hotel 1"); -- names need to be changed
-INSERT INTO `locations` (`location_id`, `location_name`) VALUES (2, "Hotel 2");
-INSERT INTO `locations` (`location_id`, `location_name`) VALUES (3, "Hotel 3");
-INSERT INTO `locations` (`location_id`, `location_name`) VALUES (4, "Hotel 4");
+-- insert necessary hotel data
+INSERT INTO `hotels` (`hotel_id`, `hotel_name`) VALUES (1, "Hotel 1"); -- names need to be changed
+INSERT INTO `hotels` (`hotel_id`, `hotel_name`) VALUES (2, "Hotel 2");
+INSERT INTO `hotels` (`hotel_id`, `hotel_name`) VALUES (3, "Hotel 3");
+INSERT INTO `hotels` (`hotel_id`, `hotel_name`) VALUES (4, "Hotel 4");
 
 -- insert necessary room sizes table
 INSERT INTO `room_sizes` (`room_size_id`, `room_size_name`) VALUES (1, "Double Full Beds");
