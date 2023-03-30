3b346a362aec52117cabc0581418f8f94f107dc7
Taylor Reid <taylorsreid@gmail.com>
2023-03-29 20:01:00 -1000

    Fixed crossorigin again lol.

--- api/src/main/java/provisio/api/MainController.java
+++ api/src/main/java/provisio/api/MainController.java
@@ -6,6 +6,7 @@ import provisio.api.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequestMapping(path = "/Provisio/api")
 @RestController
 public class MainController {
@@ -17,19 +18,16 @@ public class MainController {
     @Autowired
     private ReservationService reservationService;
 
-    @CrossOrigin
     @PostMapping(path = "/register", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest){
         return registerService.register(registerRequest);
     }
 
-    @CrossOrigin
     @PostMapping(path = "/login", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
         return loginService.login(loginRequest);
     }
 
-    @CrossOrigin
     @PostMapping(path = "/reservations/new", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<String> createReservation(
             @RequestHeader(value = HttpHeaders.AUTHORIZATION) String authorizationHeader,
@@ -38,15 +36,16 @@ public class MainController {
         return reservationService.post(authorizationHeader, reservationPostRequest);
     }
 
-    @CrossOrigin
+    //authorization header is optional for now because it's not listed in the project requirements that the user be
+    //logged in to retrieve by reservation ID
     @PostMapping(path = "/reservations/getByReservationId", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<String> getReservationByReservationId(@RequestBody ReservationGetByReservationIdRequest reservationGetByReservationIdRequest){
         return reservationService.getByReservationId(reservationGetByReservationIdRequest);
     }
 
-    @CrossOrigin
     @PostMapping(path = "/reservations/getByUserId", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<String> getReservationByUserId(@RequestHeader(value = HttpHeaders.AUTHORIZATION) String authorizationHeader){
             return reservationService.getByUserId(authorizationHeader);
     }
+
 }
--- js/apiLocation.js
+++ js/apiLocation.js
@@ -1,3 +1,4 @@
 //storing this in a separate location makes the API location easily movable
-const apiLocation = "https://taylorsreid.com:8443/Provisio/api/";
+// const apiLocation = "https://taylorsreid.com:8443/Provisio/api/";
+const apiLocation = "https://192.168.1.219:8443/Provisio/api";
 export default apiLocation;
