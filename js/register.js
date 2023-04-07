fc7143ddd19caf8a1d929ef6c0557f3da28455ec
Taylor Reid <taylorsreid@gmail.com>
2023-04-07 02:45:12 -1000

    Consolidate CSS, completed login and registration pages.

--- js/register.js
+++ js/register.js
@@ -2,13 +2,16 @@ import apiLocation from "./apiLocation.js";
 import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/+esm';
 
 //assign easy to use variables
-let form = document.getElementById("form");
-let submitButton = document.getElementById("submitButton");
-let formMessage = document.getElementById("formMessage");
+const container = document.getElementById("container");
+const form = document.getElementById("form");
+const formMessage = document.getElementById("formMessage");
 
 //if user is already logged in then replace page body with a message. Cookies returns string so compare to boolean as a string.
 if(Cookies.get("loggedIn") === "true"){
-    form.innerHTML = `You are already logged in as ${Cookies.get("firstName")} ${Cookies.get("lastName")}.`;
+    container.innerHTML = `
+                            <p>You are already logged in as ${Cookies.get("firstName")} ${Cookies.get("lastName")}.</p>
+                            <p>Did you wish to <a href="./logout.html">logout</a>?</p>
+                        `;
 }
 
 form.addEventListener('submit', function(event){
@@ -19,30 +22,40 @@ form.addEventListener('submit', function(event){
 
     const registerValues = Object.fromEntries(data.entries()); //convert form data to JS object
 
+    //if first password entry and confirm password entry match, build and send request to API
+    //all other validation is done on backend
+    if (registerValues.password === document.getElementById("confirmPassword").value) {
+        fetch(apiLocation + 'register', {
+            method : "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify(registerValues) //stringify object for request to API
+        })
+        .then(resp => resp.json())
+        .then(json => {
+            //if the API responds that the account was created successfully, then save commonly accessed items to a cookie to avoid repeated database calls
+            //API responds to successful register calls with a login request response
+            if (json.success == true) {
+                //save commonly accessed items to a cookie to avoid repeated database calls, {expires : 1} sets the cookie to be valid for one day
+                Cookies.set("loggedIn", true, {expires : 1});
+                Cookies.set("email", email, {expires : 1}); //not sent back by the API since the user has already entered it
+                Cookies.set("firstName", json.firstName, {expires : 1});
+                Cookies.set("lastName", json.lastName, {expires : 1});
+                Cookies.set('jwt', json.jwt, {expires : 1}) //save JWT to a cookie because it's the most secure way to store them
+                window.location.href = "./index.html"; //redirect back to home page upon successful login
+            }
+            else {
+                formMessage.innerHTML = json.message;
+            }
+        })
+    } else {
+        formMessage.innerHTML = "Passwords do not match."
+    }
 
-    //build request to API
-    fetch(apiLocation + 'register', {
-        method : "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body : JSON.stringify(registerValues) //stringify object for request to API
-    })
-    .then(resp => resp.json())
-    .then(json => {
-        //if the API responds that the account was created successfully, then save commonly accessed items to a cookie to avoid repeated database calls
-        //API responds to successful register calls with a login request response
-        if (json.success == true) {
-             //save commonly accessed items to a cookie to avoid repeated database calls, {expires : 1} sets the cookie to be valid for one day
-             Cookies.set("loggedIn", true, {expires : 1});
-             Cookies.set("email", email, {expires : 1}); //not sent back by the API since the user has already entered it
-             Cookies.set("firstName", json.firstName, {expires : 1});
-             Cookies.set("lastName", json.lastName, {expires : 1});
-             Cookies.set('jwt', json.jwt, {expires : 1}) //save JWT to a cookie because it's the most secure way to store them
-             window.location.href = "./index.html"; //redirect back to home page upon successful login
-        }
-        else {
-            formMessage.innerHTML = json.message;
-        }
-    })
+});
+
+//clear warning message upon form reset
+document.getElementById("secondaryButton").addEventListener('click', function(){
+    formMessage.innerHTML = "";
 });
