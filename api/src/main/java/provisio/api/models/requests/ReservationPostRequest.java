ddf763477ef973f0984c1e6e30f3b1c510e8547a
Taylor Reid <taylorsreid@gmail.com>
2023-04-12 14:51:27 -1000

    Rename parameters in request and responses.

--- api/src/main/java/provisio/api/models/requests/ReservationPostRequest.java
+++ api/src/main/java/provisio/api/models/requests/ReservationPostRequest.java
@@ -3,6 +3,7 @@ package provisio.api.models.requests;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
+import lombok.Setter;
 import provisio.api.models.Guest;
 import java.util.ArrayList;
 
@@ -11,16 +12,17 @@ import java.util.ArrayList;
  * For use in the hotel reservation page, requirement 5.
  */
 @Getter
+@Setter
 public class ReservationPostRequest {
 
-    String hotel;
-    String checkIn; //Use a string, not a date.  Java and MySQL have compatibility issues between their date objects but strings always work.
-    String checkOut; //Use a string, not a date.  Java and MySQL have compatibility issues between their date objects but strings always work.
-    String roomSize;
-    boolean wifi;
-    boolean breakfast;
-    boolean parking;
-    ArrayList<Guest> guests;
+    private String hotelName;
+    private String checkIn; //Use a string, not a date.  Java and MySQL have compatibility issues between their date objects but strings always work.
+    private String checkOut; //Use a string, not a date.  Java and MySQL have compatibility issues between their date objects but strings always work.
+    private String roomSizeName;
+    private boolean wifi;
+    private boolean breakfast;
+    private boolean parking;
+    private ArrayList<Guest> guests;
 
     @Override
     public String toString() {
