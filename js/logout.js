938e7d9fa8608ea0d881362b852c3ef82cd29fee
Taylor Reid <taylorsreid@gmail.com>
2023-04-12 20:46:44 -1000

    Remove unnecessary files and add gitattributes.

--- js/logout.js
+++
@@ -1,8 +0,0 @@
-import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/+esm';
-//remove site related cookies and clear session storage upon logout
-Cookies.remove("loggedIn");
-Cookies.remove("email");
-Cookies.remove("firstName");
-Cookies.remove("lastName");
-Cookies.remove("jwt")
-sessionStorage.clear(); //for future use
