a02666439750cf7a7fcc27cdef509947e66bea0d
Taylor Reid <taylorsreid@gmail.com>
2023-04-13 04:43:42 -1000

    book reservation page functional

--- js/bookReservation.js
+++ js/bookReservation.js
@@ -1,64 +1,127 @@
 import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/+esm';
 
+const container = document.getElementById("container");
+const hotelName = document.getElementById("hotelName");
+const roomSizeName = document.getElementById("roomSizeName");
+const hotelImages = document.getElementsByClassName("hotelImg");
+const roomSizeImages = document.getElementsByClassName("roomSizeImg");
+const checkInInput = document.getElementById("checkIn");
+const checkOutInput = document.getElementById("checkOut");
 const form = document.getElementById("form");
 const formMessage = document.getElementById("formMessage");
-const addGuestButton = document.getElementById("addGuestButton");
+const addGuestButton = document.getElementById("addGuest");
 
 //if user is not logged in. Cookies returns string.
 if(Cookies.get("loggedIn") !== "true"){
-    document.getElementById("form").innerHTML = `You must be logged in to make a reservation, click <a href="./login.html">here</a> if you are not redirected in 3 seconds...`;
+    container.innerHTML = `You must be logged in to make a reservation, click <a href="./login.html">here</a> if you are not redirected in 3 seconds...`;
     setTimeout(function(){window.location.href = "./login.html"}, 3000); //callback to wait 3 seconds then redirect
 }
 
-//create today and tomorrow date objects
-const today = new Date();
-const tomorrow =  new Date()
-tomorrow.setDate(today.getDate() + 1)
+const steps = [1, 2, 3, 4];
+let currentStep = 1;
+function advanceStep() {
+    currentStep++;
+    document.getElementById(`step${currentStep}`).hidden = false;
+    document.getElementById(`step${currentStep}`).scrollIntoView();
+}
 
-document.getElementById("checkIn").min = today.toISOString().split("T")[0]; //set minimum check in date to today
-document.getElementById("checkOut").min = tomorrow.toISOString().split("T")[0]; //set minimum check out date to tomorrow
-
-document.getElementById("checkIn").addEventListener("change", function(){updatePoints();});
-document.getElementById("checkOut").addEventListener("change", function(){updatePoints();});
-
+let nights;
 function updatePoints(){
-    let checkIn = new Date(document.getElementById("checkIn").value);
-    let checkOut = new Date(document.getElementById("checkOut").value);
+    let checkInDate = new Date(checkInInput.value);
+    let checkOutDate = new Date(checkOutInput.value);
 
-    if(!isNaN(checkIn.getUTCFullYear()) && !isNaN(checkOut.getUTCFullYear())){ //prevents a null error from being thrown in the console
-        let deltaDays = (checkOut - checkIn) / (1000 * 3600 * 24);
-        formMessage.innerHTML = `You will earn ${deltaDays * 150} points for this trip.`;
+    if(!isNaN(checkInDate.getUTCFullYear()) && !isNaN(checkOutDate.getUTCFullYear())){ //prevents a null error from being thrown in the console
+        nights = (checkOutDate - checkInDate) / (1000 * 3600 * 24);
+        formMessage.innerHTML = `You will earn ${nights * 150} points for this trip.`;
     }
 }
 
-let currentGuest = 2;
-addGuestButton.addEventListener("click", function(){
-    if(currentGuest <= 5){
-        addGuestButton.hidden = false;
-        document.getElementById(`guest${currentGuest}`).hidden = false;
-        currentGuest++;
-        if (currentGuest === 6){
-            addGuestButton.hidden = true;
-        }
+//step 1 - choose your hotel
+Array.from(hotelImages).forEach(element => {
+    element.addEventListener("click", function(){
+        hotelName.value = element.getAttribute("hotelName");
+        advanceStep();
+    })
+});
+
+//step 2 -choose dates
+//create today and tomorrow date objects
+const today = new Date();
+const tomorrow =  new Date();
+tomorrow.setDate(today.getDate() + 1);
+checkInInput.min = today.toISOString().split("T")[0]; //set minimum check in date to today
+checkOutInput.min = tomorrow.toISOString().split("T")[0]; //set minimum check out date to tomorrow
+//if both date fields are filled out, reveal the next step
+checkInInput.addEventListener("change", function(){
+    updatePoints();
+    if (checkOutInput.value !== "") {
+        advanceStep();
+    }
+});
+checkOutInput.addEventListener("change", function(){
+    updatePoints();
+    if (checkInInput.value !== "") {
+        advanceStep();
     }
 });
 
-for (let i = 2; i <= 5; i++) {
-    document.getElementById(`removeGuestButton${i}`).addEventListener("click", function() {
-        document.getElementById(`guest${i}FirstName`).value = null;
-        document.getElementById(`guest${i}LastName`).value = null;
-        document.getElementById(`guest${i}`).hidden = true;
-        addGuestButton.hidden = false;
-        currentGuest--;
-    });
-}
+//step 3 - pick your room size
+Array.from(roomSizeImages).forEach(element => {
+    element.addEventListener("click", function(){
+        roomSizeName.value = element.getAttribute("roomSizeName");
+        advanceStep();
+    })
+});
 
+//step 4 - enter guest names
+let guestCount = 2;
+addGuestButton.addEventListener("click", function () {
+        //output additional guest name fields
+        document.getElementById("guestsBox").insertAdjacentHTML("beforeend", `
+            Guest ${guestCount}:
+            <input type="text" id="firstName${guestCount}" placeholder="First Name">
+            <input type="text" id="lastName${guestCount}" placeholder="Last Name">
+        `)
+        guestCount++;
+        if (guestCount >= 6) { //hides self (add guest button) if maximum guests is reached
+            addGuestButton.hidden = true;
+        }
+});
+
+//handle submit event
 form.addEventListener('submit', function(event){
-    event.preventDefault();
+    event.preventDefault(); //prevent default event, which is reload page if no action is set in the HTML
 
-    const data = new FormData(event.target);
+    const formData = new FormData(event.target);
 
-    const value = Object.fromEntries(data.entries());
+    let guests = [];
 
-    console.log(JSON.stringify(value));
+    for (let i = 1; i < guestCount; i++) {
+        
+        let guest = {
+            firstName : "",
+            lastName : ""
+        };
+
+        let firstName = document.getElementById(`firstName${i}`).value;
+        let lastName = document.getElementById(`lastName${i}`).value;
+
+        if (firstName !== "" && lastName !== "") {
+            guest.firstName = firstName;
+            guest.lastName = lastName;
+            guests.push(guest);
+        }
+
+    }
+
+    const formJson = Object.fromEntries(formData.entries());
+    formJson.guests = guests;
+
+    console.log(JSON.stringify(formJson));
+
+    sessionStorage.setItem("reservationInfo", JSON.stringify(formJson));
+    sessionStorage.setItem("nights", nights); //not sent to API but saved for convenience
+
+    window.location.href = "./confirmReservation.html"
+
 })
