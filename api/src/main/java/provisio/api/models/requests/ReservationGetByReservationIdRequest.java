cacb68e7637ad7da81f97cdbbcee3a7115f9ebc8
Taylor Reid <taylorsreid@gmail.com>
2023-04-14 10:44:21 -1000

    Added prices endpoint to query prices.

--- api/src/main/java/provisio/api/models/requests/ReservationGetByReservationIdRequest.java
+++ api/src/main/java/provisio/api/models/requests/ReservationGetByReservationIdRequest.java
@@ -1,25 +1,15 @@
 package provisio.api.models.requests;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
+import provisio.api.models.AbstractRequestResponse;
 
 /**
  * Model object for a JSON get reservations by reservation ID request body.
  * For use in the reservation look up page, requirement 9.
  */
 @Getter
-public class ReservationGetByReservationIdRequest {
+public class ReservationGetByReservationIdRequest extends AbstractRequestResponse {
 
     String reservationId;
-
-    @Override
-    public String toString() {
-        try {
-            return new ObjectMapper().writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 }
