2e7eb64da052d68b04dfce28fb90fa5405da28e3
Taylor Reid <taylorsreid@gmail.com>
2023-04-15 23:06:58 -1000

    confirm reservation page may show totals?

--- js/confirmReservation.js
+++ js/confirmReservation.js
@@ -9,17 +9,77 @@ if(Cookies.get("loggedIn") !== "true"){
 }
 
 //retrieve reservation info from session storage
-const reservationInfo = JSON.parse(sessionStorage.getItem("reservationInfo"));
+const requestedReservation = JSON.parse(sessionStorage.getItem("reservationInfo"));
 const nights = parseInt(sessionStorage.getItem("nights")); //not sent to API but saved for convenience
 
 //convert ISO 8601 date to American format
-const checkInLocalDate = reservationInfo.checkIn.split("-")[1] + "/" + reservationInfo.checkIn.split("-")[2] + "/" + reservationInfo.checkIn.split("-")[0];
-const checkOutLocalDate = reservationInfo.checkOut.split("-")[1] + "/" + reservationInfo.checkOut.split("-")[2] + "/" + reservationInfo.checkOut.split("-")[0];
+const checkInLocalDate = requestedReservation.checkIn.split("-")[1] + "/" + requestedReservation.checkIn.split("-")[2] + "/" + requestedReservation.checkIn.split("-")[0];
+const checkOutLocalDate = requestedReservation.checkOut.split("-")[1] + "/" + requestedReservation.checkOut.split("-")[2] + "/" + requestedReservation.checkOut.split("-")[0];
 
-document.getElementById("hotelName").innerHTML = reservationInfo.hotelName;
+//fill in check in and check out dates
 document.getElementById("checkIn").innerHTML = checkInLocalDate;
 document.getElementById("checkOut").innerHTML = checkOutLocalDate;
-document.getElementById("roomSizeName").innerHTML = reservationInfo.roomSizeName;
-document.getElementById("wifi").innerHTML = reservationInfo.wifi;
-document.getElementById("breakfast").innerHTML = reservationInfo.breakfast;
-document.getElementById("parking").innerHTML = reservationInfo.parking;
+
+//fill in hotel name
+document.getElementById("hotelName").innerHTML = requestedReservation.hotelName;
+
+//fill in room size name
+document.getElementById("roomSizeName").innerHTML = requestedReservation.roomSizeName;
+
+//fill in nights
+for (const element of document.getElementsByClassName("nights")){
+    element.innerHTML = nights;
+}
+
+//retrieve all prices from API
+//prices retrieved from API instead of hard coded to allow for future price changes
+fetch(apiLocation + 'prices', {
+    method : "POST",
+    headers: {
+        'Content-Type': 'application/json',
+    }
+})
+.then(resp => resp.json())
+.then(json => {
+    console.log(JSON.stringify(json));
+    if (json.success == true) {
+
+        let grandTotal = 0;
+
+        //update room price
+        const roomTotal = (nights * parseFloat(json.prices[requestedReservation.roomSizeName])); //per night
+        document.getElementById("roomTotal").innerHTML = `$${roomTotal}`;
+        grandTotal += roomTotal;
+
+        //unhide wifi row and update prices
+        if (requestedReservation.wifi) {
+            const wifiTotal = parseFloat(json.prices.wifi); //flat rate, not per night
+            document.getElementById("wifiRow").hidden = false;
+            document.getElementById("wifiTotal").innerHTML = `$${wifiTotal}`;
+            grandTotal += wifiTotal;
+        }
+
+        //unhide breakfast row and update prices
+        if (requestedReservation.breakfast) {
+            const breakfastTotal = (nights * parseFloat(json.prices.breakfast)); //per night
+            document.getElementById("breakfastRow").hidden = false;
+            document.getElementById("breakfastTotal").innerHTML = `$${breakfastTotal}`;
+            grandTotal += breakfastTotal;
+        }
+
+        //unhide parking row and update prices
+        if (requestedReservation.parking) {
+            const parkingTotal = (nights * parseFloat(json.prices.parking)); //per night
+            document.getElementById("parkingRow").hidden = false;
+            document.getElementById("parkingTotal").innerHTML = `$${parkingTotal}`;
+            grandTotal += breakfastTotal;
+        }
+
+        //render grand total
+        document.getElementById("grandTotal").innerHTML = `$${grandTotal}`;
+        
+    }
+    else{
+
+    }
+});
