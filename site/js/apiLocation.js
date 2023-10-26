ea8ffb4ad9f3c0f265bb7edae3fdc0f69a185aa5
Taylor Reid <taylorsreid@gmail.com>
2023-10-25 22:23:00 -1000

    update apiLocation

--- site/js/apiLocation.js
+++ site/js/apiLocation.js
@@ -1,3 +1,3 @@
 //storing this in a separate location makes the API location easily movable
-const apiLocation = "https://provisio-api.taylorsreid.com/";
+const apiLocation = "https://provisio-api.up.railway.app";
 export default apiLocation;
