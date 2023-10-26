26e1741b98f6b82743a22e51d2c79464b85b226e
Taylor Reid <taylorsreid@gmail.com>
2023-10-25 22:36:57 -1000

    fix missing slash in apiLocation

--- site/js/apiLocation.js
+++ site/js/apiLocation.js
@@ -1,3 +1,3 @@
 //storing this in a separate location makes the API location easily movable
-const apiLocation = "https://provisio-api.up.railway.app";
+const apiLocation = "https://provisio-api.up.railway.app/";
 export default apiLocation;
